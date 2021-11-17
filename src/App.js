c7df330a6ac562544eec9b187edc5c0a59206dd3
kiran <kiranbits199@gmail.com>
2021-11-17 19:13:34 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -5,14 +5,14 @@ import './App.css';
 
 import Leftmenu from './Leftmenu';
 import Header from './Header';
-import Mainpanel from './Mainpanel';
+
 
 function App() {
   return (
     <div>
      <Header />
     <Leftmenu />
-    <Mainpanel />
+  
       
     </div>
     );
